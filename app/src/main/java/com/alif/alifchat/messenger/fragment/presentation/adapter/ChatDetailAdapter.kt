2ebe05a82b85845c8e9d278706a33e23af61bdb7
farizajaborova <farizaposho@gmail.com>
2024-03-14 15:18:27 +0500

    fixed bug

--- app/src/main/java/com/alif/alifchat/messenger/fragment/presentation/adapter/ChatDetailAdapter.kt
+++ app/src/main/java/com/alif/alifchat/messenger/fragment/presentation/adapter/ChatDetailAdapter.kt
@@ -1,5 +1,7 @@
 package com.alif.alifchat.messenger.fragment.presentation.adapter
 
+import android.content.Intent
+import android.net.Uri
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
